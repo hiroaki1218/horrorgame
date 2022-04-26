510cc9ca4213f1701ca26d018e05aa1f9d341f3d
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-04-27 02:43:08 +0900

    Merge branch 'hiroaki' of https://github.com/hiroaki1218/horrorgame
    # Conflicts:
    #	Assets/Scenes/MainScene.unity

--- Assets/C#programs/Menu.cs
+++ Assets/C#programs/Menu.cs
@@ -6,7 +6,8 @@ using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
 
     public GameObject player;
     public GameObject OnPanel, OnUnPanel;
@@ -17,9 +18,9 @@ public class Menu : MonoBehaviour {
     //雷の音　(Clip)
     //private float rainvolume = 0.2f;
 
-    private bool esc1;
+    private bool pauseGame = false;
     //private bool esc1 = false;
-    
+
     void Start()
     {
         OnUnPause();
@@ -28,51 +29,60 @@ public class Menu : MonoBehaviour {
         //雷の音　(Clip)
         audioSouse2.volume = rainvolume;
         audioSouse2.Play();
-    }  
+    }
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            
+            pauseGame = !pauseGame;
 
-            if ( esc1 == false )
+<<<<<<< HEAD
+            if ( pauseGame == true)
+=======
+            if (pauseGame == true)
+>>>>>>> a98276b5c018a254427d74285f982b48fc918013
             {
                 OnPause();
             }
-            if ( esc1 == true )
+            else
             {
                 OnUnPause();
                 Debug.Log("now pause");
             }
-        }    
+        }
+<<<<<<< HEAD
+         
+=======
+
+>>>>>>> a98276b5c018a254427d74285f982b48fc918013
     }
 
     public void OnPause()
-    {   
+    {
         OnPanel.SetActive(true);        // PanelMenuをtrueにする
         OnUnPanel.SetActive(false);     // PanelEscをfalseにする
         audioSource1.Pause();
         audioSouse2.Pause();
         Time.timeScale = 0;
-        esc1 = true;
+        pauseGame = true;
         FirstPersonController fpc = player.GetComponent<FirstPersonController>();
         fpc.enabled = false;
-        
+
         Cursor.lockState = CursorLockMode.None;     // 標準モード
         Cursor.visible = true;    // カーソル表示
     }
 
     public void OnUnPause()
-    {   
-        
+    {
+
         OnPanel.SetActive(false);       // PanelMenuをfalseにする
         OnUnPanel.SetActive(true);      // PanelEscをtrueにする
         audioSource1.Play();
         //雷の音　(Clip)
         audioSouse2.Play();
         Time.timeScale = 1;
-        esc1 = false;
+        pauseGame = false;
         FirstPersonController fpc = player.GetComponent<FirstPersonController>();
         fpc.enabled = true;
 
@@ -80,13 +90,15 @@ public class Menu : MonoBehaviour {
         Cursor.visible = false;     // カーソル非表示
     }
 
-    public void button(){
+    public void button()
+    {
 
-    var Clicked = BuckButton.GetComponent<Button>();
+        var Clicked = BuckButton.GetComponent<Button>();
         Clicked.onClick.AddListener(buck);
     }
 
-    public void buck(){
+    public void buck()
+    {
         OnUnPause();
     }
 
