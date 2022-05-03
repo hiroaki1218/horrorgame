0190bfad3ccbfee5bdd6a314cdf365a720a59cef
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-05-03 16:41:55 +0900

    10分カウントダウンバー追加！

---
+++ Assets/C#programs/CountDown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] float gameTime = 600.0f;
+    [SerializeField] private GameObject countUI;
+    [SerializeField] private Slider slider;
+    float maincount;
+    float second;
+    
+
+    private void Start()
+    {
+        slider.value = 1;
+        countUI.SetActive(true);
+        maincount = gameTime;
+    }
+    private void Update()
+    {
+        if(maincount >= 0.0f)
+        {
+            maincount -= Time.deltaTime;
+            second = maincount / gameTime;
+            slider.value = second;
+        }
+        //Debug.Log(maincount);
+    }
+}
