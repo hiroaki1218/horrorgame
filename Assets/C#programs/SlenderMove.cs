641a0d42d9546f40abd201e5d6944a98a6e9879d
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-06-07 23:55:29 +0900

    スレンダーちょい修正

--- Assets/C#programs/SlenderMove.cs
+++ Assets/C#programs/SlenderMove.cs
@@ -48,28 +48,34 @@ public class SlenderMove : MonoBehaviour
         headToPlayerDir = Vector3.Normalize(headToPlayerDir);
         float dot = Vector3.Dot(headToPlayerDir, head.forward);
 
-        if (dot > 0)
-        {
-            increaseAttention += 0.05f * dot;
-            text.text = "\nA:見えている";
+        Debug.Log(dot);
 
-            //プレイヤーが走っていないとき
-            if (sc_fps.GetIsWalking())
+        if(distanceFromPlayer <= 40)
+        {
+            if (dot > 0)
             {
-                text.text = "\nP:走っていない";
-                attention -= 0.008f; // 注意のレベルを下げる            
+                increaseAttention += 0.02f * dot;
+                text.text = "\nA:見えている";
+
+                //プレイヤーが走っていないとき
+                if (sc_fps.GetIsWalking())
+                {
+                    text.text = "\nP:走っていない";
+                    attention += 0.008f; // 注意のレベルを下げる            
+                }
+                //プレイヤーが走っているとき
+                else
+                {
+                    text.text = "\nP:走っている";
+                    increaseAttention += 0.01f; // 注意のレベルを上げる
+                    increaseAttention *= Mathf.Clamp(5f / distanceFromPlayer, 0f, 1f);
+                }
             }
-            //プレイヤーが走っているとき
             else
             {
-                text.text = "\nP:走っている";
-                increaseAttention += 0.03f; // 注意のレベルを上げる
-                increaseAttention *= Mathf.Clamp(3f / distanceFromPlayer, 0f, 1f);
+                text.text = "\nA:見えていない";
+                attention -= 0.02f;
             }
-        }
-        else
-        {
-            text.text = "\nA:見えていない";
         }
 
         // 追っていない状態
@@ -88,17 +94,12 @@ public class SlenderMove : MonoBehaviour
                 ///attention = 1f;
                 state = 1;
                 agent.destination = player.position; // プレイヤーの場所に行く
-                //Destroy(sphere);
-                //sphere = Instantiate(spherePrefab, player.position, spherePrefab.transform.rotation);
             }
             // 目的地付近で次の目的地を設定
             else if (!agent.pathPending && agent.remainingDistance < 0.5f)
             {
                 Vector3 dest = GetDestinationRandomly();
                 agent.destination = dest;
-                //Destroy(sphere);
-                //sphere = Instantiate(spherePrefab, dest, spherePrefab.transform.rotation);
-
             }
 
         }
@@ -108,13 +109,11 @@ public class SlenderMove : MonoBehaviour
 
             text.text += "\nA:追っている";
             agent.destination = player.position; // プレイヤーの場所に行く
-                                                 // Destroy(sphere);
-                                                 //sphere = Instantiate(spherePrefab, player.position, spherePrefab.transform.rotation);
 
             // 1以上のとき1
             if (attention >= 1f)
             {
-                ///attention = 1f;
+                attention = 1f;
             }
             // 見失っている状態にする
             else
@@ -131,19 +130,15 @@ public class SlenderMove : MonoBehaviour
             // 1以上のとき追っている状態にする
             if (attention >= 1f)
             {
-                ///attention = 1f;
+                attention = 1f;
                 state = 1;
                 agent.destination = player.position; // プレイヤーの場所に行く
-                //Destroy(sphere);
-                //sphere = Instantiate(spherePrefab, player.position, spherePrefab.transform.rotation);
             }
             // 0.4で追ってない状態にする
             else if (attention <= 0.4f)
             {
                 Vector3 dest = GetDestinationRandomly();
                 agent.destination = dest;
-                //Destroy(sphere);
-                //sphere = Instantiate(spherePrefab, dest, spherePrefab.transform.rotation);
                 state = 0;
             }
             // 目的地付近で次の目的地
@@ -151,8 +146,6 @@ public class SlenderMove : MonoBehaviour
             {
                 Vector3 dest = GetDestinationRandomly();
                 agent.destination = dest;
-                //Destroy(sphere);
-                //sphere = Instantiate(spherePrefab, dest, spherePrefab.transform.rotation);
                 state = 0;
             }
         }
