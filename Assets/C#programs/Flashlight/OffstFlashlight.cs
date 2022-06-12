76c1328dff339f953aeba7c9c3e96ac686ca184b
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-06-12 18:20:14 +0900

    UI関係 & メモ1 & バッテリー緑色に発光

---
+++ Assets/C#programs/Flashlight/OffstFlashlight.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OffstFlashlight : MonoBehaviour
+{
+    //[SerializeField] private GameObject SubCamera;
+    [SerializeField] private Light Fpslight;
+    private Vector3 vectOffset;
+    private GameObject goFollow;
+    [SerializeField] private float speed = 5.0f;
+
+    [SerializeField] private GameObject FlashlightUI;
+    [SerializeField] private Image bar;
+    [SerializeField] private float maxglowTime = 100.0f;
+    [SerializeField] private float Glowingtime = 100.0f;
+    [SerializeField] private float decrease = 2.0f;
+    [SerializeField] private float batteryRecovery = 20.0f;
+    [HideInInspector] public bool isRemain;
+    
+
+    public static OffstFlashlight instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        isRemain = true;
+        goFollow = Camera.main.gameObject;
+        vectOffset = transform.position - goFollow.transform.position;
+        Fpslight.enabled = false;
+        FlashlightUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        bar.fillAmount = Glowingtime / maxglowTime;
+        Debug.Log(Glowingtime / maxglowTime);
+        
+        if (PickupObj.fpsLight && isRemain && !PhoneAnimation.isLookPhone)
+        {
+             FlashlightUI.SetActive(true);
+             Fpslight.enabled = true;
+        }
+        else
+        {
+             FlashlightUI.SetActive(false);
+             Fpslight.enabled = false;
+        }
+        
+        transform.position = goFollow.transform.position + vectOffset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, goFollow.transform.rotation, speed * Time.deltaTime);
+
+        //光る時間を徐々に減らす&電池残量の確認
+        if (PickupObj.fpsLight && !PhoneAnimation.isLookPhone)
+        {
+            if(Glowingtime >= 0)
+            {
+                Glowingtime -= decrease * Time.deltaTime;
+                isRemain = true;
+            }
+            else
+            {
+                isRemain = false;
+            }
+        }
+        
+    }
+    //バッテリーの概念
+    public void SetBattery()
+    {
+        if(Glowingtime <= maxglowTime - 0.01f)
+        {
+            if((Glowingtime += batteryRecovery) > maxglowTime)
+            {
+                Glowingtime = maxglowTime;
+            }
+        }
+    }
+}
---
+++ Assets/C#programs/Item/Items.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Items
+{
+    //public GameObject item;
+    //public string expalianText;
+
+    //—ñ‹“Œ^Fí—Ş‚ğ’è‹`
+    public enum Type
+    {
+        Key,
+        Flashlight,
+        Phone,
+        Battery,
+        Memo1,
+    }
+
+    //ItemType‚ğéŒ¾
+    public Type type;
+    //Item‰æ‘œ‚ğéŒ¾
+    public Sprite sprite;
+
+    public Items(Type type, Sprite sprite)
+    {
+        this.type = type;
+        this.sprite = sprite;
+    }
+}
