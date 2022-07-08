b5afe02fde69791db72d0e2a9c24e3c07a81d626
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-07-09 00:37:58 +0900

    ハンマー関係（スクリプトのみ）

---
+++ Assets/C#programs/HammerAnimation.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class HammerAnimation : MonoBehaviour
+{
+    [SerializeField] private GameObject FirstPerson;
+    Animator _anim;
+    FirstPersonControllerCustom _fpc;
+    public static HammerAnimation instance;
+    public bool Action;
+    private bool canShake;
+    private bool canClick;
+    public static bool isHammerShake;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        Action = false;
+        canShake = false;
+        canClick = true;
+        isHammerShake = false;
+        _anim = FirstPerson.GetComponent<Animator>();
+        _fpc = FirstPerson.GetComponent<FirstPersonControllerCustom>();
+    }
+    void OnClickYesButon()
+    {
+        Action = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Action && canClick && Input.GetMouseButton(1))
+        {
+            Action = false;
+        }
+        ChangeAction();
+        StartCoroutine("HammerShake");
+    }
+    void ChangeAction()
+    {
+        if (Action)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (canClick)
+                {
+                    canShake = true;
+                }
+            }
+        }
+    }
+    IEnumerator HammerShake() 
+    {
+        if (canShake)
+        {
+            canClick = false;
+            isHammerShake = true;
+            _anim.Play("FPSHammerShake");
+            yield return new WaitForSeconds(0.5f);
+            isHammerShake = false;
+            canClick = true;
+            canShake = false;
+        }
+        else if(Action && !canShake)
+        {
+            if (_fpc.m_IsStopping)
+            {
+                _anim.Play("FPSHammerHaving(Idle)");
+            }
+            else if (_fpc.m_IsWalking)
+            {
+                _anim.Play("FPSHammerHaving(Walk)");
+            }
+            else if (_fpc.m_IsRunning)
+            {
+                _anim.Play("FPSHammerHaving(Run)");
+            }
+           
+        }
+    }
+}
---
+++ Assets/C#programs/HammerBreak.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerBreak : MonoBehaviour
+{
+    [SerializeField] private GameObject Parents;
+    [SerializeField] private GameObject[] BreakObject;
+    [SerializeField] private int MaxBreakObj;
+    int count;
+    int breaknumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        breaknumber = count - 1;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            count++;
+            breaknumber = count - 1;
+        }
+        if (breaknumber >= 0)
+        {
+            if (count == MaxBreakObj)
+            {
+                Destroy(Parents);
+            }
+            else
+            {
+                BreakObject[breaknumber].SetActive(false);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
