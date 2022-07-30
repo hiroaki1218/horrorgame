635554530b0b7957a3a060d6163f2cacd80a745d
NozawaHiroaki <hongzhangyeze@gmail.com>
2022-07-30 23:54:33 +0900

    直った

---
+++ Assets/C#programs/MemoChest/MemoChest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoChest : MonoBehaviour
+{
+    public static MemoChest instance;
+
+    [SerializeField] private GameObject Memo6;
+    [SerializeField] private GameObject HomeKey;
+    public bool canClickButton;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        Memo6.SetActive(false);
+        HomeKey.SetActive(false);
+        canClickButton = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            canClickButton = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            canClickButton = false;
+        }
+    }
+}
---
+++ Assets/C#programs/MemoChest/NumKey.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumKey : MonoBehaviour
+{
+    [SerializeField] private Text displayText;
+    public string TrueThirdRank;
+    public string TrueSecondRank;
+    public string TrueFirstRank;
+
+    private string firstRank;
+    private string secondRank;
+    private string thirdRank;
+
+    private void Start()
+    {
+        firstRank = null;
+        secondRank = null;
+        thirdRank = null;
+        displayText.text = thirdRank + secondRank + firstRank;
+    }
+    public void OnClickNumButton(string n)
+    {
+        displayText.text = thirdRank + secondRank + firstRank;
+        if (firstRank == null)
+        {
+            firstRank = n;
+        }
+        else if(secondRank == null)
+        {
+            secondRank = n;
+        }
+        else if(thirdRank == null)
+        {
+            thirdRank = n;
+        }
+    }
+    public void OnClickBackButton()
+    {
+        displayText.text = thirdRank + secondRank + firstRank;
+        if (firstRank == null)
+        {
+            return;
+        }
+        else if(secondRank == null)
+        {
+            firstRank = null;
+        }
+        else if(thirdRank == null)
+        {
+            firstRank = null;
+            firstRank = secondRank;
+        }
+        else
+        {
+            firstRank = null;
+            firstRank = secondRank;
+            secondRank = null;
+            secondRank = thirdRank;
+        }
+    }
+    public void OnClickEnterButton()
+    {
+        StartCoroutine(Check());
+    }
+    IEnumerator Check()
+    {
+        if(thirdRank == TrueThirdRank 
+        && secondRank == TrueSecondRank
+        && firstRank == TrueFirstRank)
+        {
+            Debug.Log("正解");
+            //緑のUI
+            yield return new WaitForSeconds(1);
+            //緑のUI消す
+        }
+        else
+        {
+            //赤のUI
+            yield return new WaitForSeconds(1);
+            //赤のUI消す
+        }
+    }
+}
