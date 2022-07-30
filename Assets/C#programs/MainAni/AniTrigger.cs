9a1f16405632d8f73d6a339ad017f6dfc67c8797
hatimitu728 <mitsuru728pc@outlook.jp>
2022-07-30 23:46:50 +0900

    車追加&アニメーション途中

---
+++ Assets/C#programs/MainAni/AniTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniTrigger : MonoBehaviour
+{
+    public static bool enter;
+
+    private void Start()
+    {
+        enter = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+}
---
+++ Assets/C#programs/MainAni/CharacterAni.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CharacterAni : MonoBehaviour
+{
+    [SerializeField] private FirstPersonAnime _fpcanim;
+    [SerializeField] private FirstPersonControllerCustom FPC;
+    [SerializeField] private CharacterController fps;
+    [SerializeField] private Animator _anim;
+    [SerializeField] private Camera startCamera;
+    [SerializeField] private GameObject CrosshairUI;
+
+    private void Start()
+    {
+        _fpcanim.enabled = false;
+        startCamera.enabled = true;
+        FPC.enabled = false;
+        fps.enabled = false;
+        CrosshairUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        StartCoroutine(FPSAni());
+    }
+
+    IEnumerator FPSAni()
+    {
+        if (!AniTrigger.enter)
+        {
+            _anim.Play("BasicMotions@Walk01 - Forwards");
+            for (int walk = 0; walk < 20; walk++)
+            {
+                _anim.gameObject.transform.Translate(0, 0, 0.001f);
+                yield return new WaitForSeconds(0.01f);
+            }
+        }
+    }
+
+}
